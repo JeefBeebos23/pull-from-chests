f80a8173a3b71ffc69626b19f242f6d5b8a736a5
Aidan Guinn <wbguinn77@gmail.com>
2026-05-17 19:32:21 -0700

    fix: target AbstractContainerScreen instead of ContainerScreen for chest sort mixin

--- src/main/java/com/jeefbeebos23/pull_from_chests/mixin/ContainerScreenMixin.java
+++ src/main/java/com/jeefbeebos23/pull_from_chests/mixin/ContainerScreenMixin.java
@@ -6,9 +6,9 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,15 +16,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(ContainerScreen.class)
-public abstract class ContainerScreenMixin extends AbstractContainerScreen<ChestMenu> {
+@Mixin(AbstractContainerScreen.class)
+public abstract class ContainerScreenMixin<T extends AbstractContainerMenu>
+        extends AbstractContainerScreen<T> {
 
-    protected ContainerScreenMixin(ChestMenu menu, Inventory playerInventory, Component title) {
+    protected ContainerScreenMixin(T menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
     }
 
     @Inject(method = "init", at = @At("TAIL"))
-    private void addSortButton(CallbackInfo ci) {
+    private void addChestSortButton(CallbackInfo ci) {
+        if (!(this.menu instanceof ChestMenu)) return;
         addRenderableWidget(Button.builder(
             Component.literal("Sort"),
             btn -> ClientPlayNetworking.send(new SortChestPayload()))
